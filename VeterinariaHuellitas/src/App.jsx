dee9b0280b65e871f92aee369591ae01ed6aef0a
Pablo <pablomd_98@hotmail.com>
2023-08-24 06:57:43 -0300

    creacion nueva de la rama

--- VeterinariaHuellitas/src/App.jsx
+++ VeterinariaHuellitas/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Rutas from './components/rutas/Rutas';
+// import ProductosContexto from './components/context/ProductosContext';
+// import ContextoTurnos from './components/context/TurnosContext';
+// import ContextoMascotas from './components/context/MascotasContext';
+// import ContextoUsuarios from './components/context/UsuariosContext';
+import PaginaPrincipal from './pages/paginaPrincipal/PaginaPrincipal';
 
 
 
 const App = () => {
- 
+
 
   return (
     <>
+    <PaginaPrincipal />
+
+      {/* <Rutas /> */}
 
     </>
   )
---
+++ VeterinariaHuellitas/src/pages/paginaPrincipal/PaginaPrincipal.jsx
@@ -0,0 +1,34 @@
+import './PaginaPrincipal.css'
+import Carrusel from "../../components/carrusel/Carrusel"
+import Servicios from "../../components/servicios/servicios";
+import Profesionales from "../../components/profesionales/Profesionales"
+import Planes from "../../components/planes/Planes"
+// import Productos from "../../components/productos/Productos";
+import Slider from "../../components/slider/slider";
+// import { useContext } from "react";
+// import { ProductosContexto } from "../../components/context/ProductosContext";
+
+
+
+
+
+const PaginaPrincipal = () => {
+
+  return (
+    <>
+      <Carrusel />
+      <div className="conteinerText">
+        <p className="descripcion">
+          <strong>Somos una clínica veterinaria dedicada a proporcionar cuidados excepcionales para tus mascotas.</strong> Nuestro equipo altamente capacitado ofrece servicios médicos avanzados en instalaciones modernas. Desde <em>exámenes preventivos</em> hasta <em>tratamientos especializados</em>, cuidamos de tus animales con <em>amor y experiencia</em>. Confía en nosotros para mantener a tus mascotas saludables y felices.
+        </p>
+      </div>
+      <Servicios />
+      <Profesionales />
+      <Planes />
+      {/* <Productos /> */}
+      <Slider />
+    </>
+  )
+}
+
+export default PaginaPrincipal
