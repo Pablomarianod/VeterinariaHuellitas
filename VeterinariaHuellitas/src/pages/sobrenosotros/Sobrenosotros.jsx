a6fa3b66a9ac3fe77e1f39e826ee22001e67859e
DanielSpiner <danielspr.33@gmail.com>
2023-08-27 15:18:55 -0300

    se acomoda las cards, se cambia de color a las cards

--- VeterinariaHuellitas/src/pages/sobrenosotros/Sobrenosotros.jsx
+++ VeterinariaHuellitas/src/pages/sobrenosotros/Sobrenosotros.jsx
@@ -4,23 +4,47 @@ function About() {
   return (
     <>
       <div>
-        <h1 className="text-center my-4"><b>¿Quiénes somos?</b></h1>
+      <h2 className="text-center my-4"><b><u>¿Quiénes somos?</u></b></h2>
       </div>
 
-
-      <div className="containerAbout fach text-center p-5">
+      {/* Contenedor 1 */}
+      <div className="container fach text-center p-5">
         <p>
-          Somos un equipo de desarrolladores Full Stack apasionados por los animales y la tecnología.
-          Juntos, creamos 'Huellitas', una página de veterinaria que une nuestra experiencia en desarrollo con nuestro amor por los amigos peludos.
-          Con una interfaz amigable, permitimos a los dueños de mascotas acceder fácilmente a servicios veterinarios y programar citas.
-          Nuestra misión es mejorar la vida de las mascotas y sus dueños a través de la innovación tecnológica y el cuidado dedicado.
+          En Huellitas, comprendemos que tus mascotas son parte de tu familia.
+          Nuestro compromiso es proporcionarles el mejor cuidado veterinario en
+          un ambiente cálido y acogedor. Con años de experiencia en la atención
+          de animales de compañía, nuestro equipo de veterinarios altamente
+          capacitados se dedica a mantener a tus amigos peludos felices y
+          saludables en cada etapa de sus vidas.
+        </p>
+        <p>
+          Ofrecemos una amplia gama de servicios veterinarios diseñados para
+          satisfacer todas las necesidades de tus mascotas. Desde chequeos de
+          rutina, vacunación y desparasitación, hasta cirugías especializadas y
+          cuidados intensivos, estamos preparados para brindar la atención
+          médica que tus animales merecen. Ya sea que tengas un cachorro
+          juguetón, un gato curioso o una mascota mayor que necesita cuidados
+          especiales, estamos aquí para ayudar.
+        </p>
+        <p>
+          Nuestro equipo está formado por apasionados amantes de los animales
+          que se esfuerzan por establecer conexiones duraderas con nuestros
+          pacientes y sus dueños. Entendemos que cada mascota es única, y
+          trabajamos en colaboración contigo para desarrollar planes de atención
+          personalizados que se adapten a las necesidades individuales de tu
+          compañero peludo.
         </p>
       </div>
 
+      <div className="container my-5">
+        <hr />
+      </div>
+      {/* Fin de Contenedor 1 */}
 
-      <h2><b>Nuestro equipo:</b></h2>
+      <h2 className="text-center"><b><u>Nuestro equipo</u></b></h2>
 
-      <div className="containerCardsAbout d-flex flex-wrap justify-content-center">
+      {/* Contenedor 2 */}
+      <div className="container d-flex flex-wrap justify-content-center">
         <div className="d-flex mx-4 my-4">
           <div
             className="card fach cambiocolor"
@@ -29,9 +53,10 @@ function About() {
             <img
               src="/src/images/integrantes/pablo.jpg"
               alt="esteban"
-              className="fotoCircle mx-4 mt-4"
+              className="fotoCircle mt-4"
             />
-            <h3 className="text-center mt-4"><b>Pablo Décima</b></h3>
+            <h3 className="text-center mt-4"><b>Pablo Decima</b></h3>
+        
           </div>
         </div>
 
@@ -43,9 +68,10 @@ function About() {
             <img
               src="/src/images/integrantes/daniell.jpg"
               alt="esteban"
-              className="fotoCircle mx-4 mt-4"
+              className="fotoCircle  mt-4"
             />
             <h3 className="text-center mt-4"><b>Daniel Spiner</b></h3>
+          
           </div>
         </div>
 
@@ -57,22 +83,28 @@ function About() {
             <img
               src="/src/images/integrantes/Estebanp.jpg"
               alt="esteban"
-              className="fotoCircle mx-4 mt-4"
+              className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b>Esteban Pedraza</b></h3>
+            
           </div>
         </div>
+      </div>
+           
+
+      <div className="container d-flex flex-wrap justify-content-center">
         <div className="d-flex mx-5 my-4">
           <div
             className="card fach cambiocolor"
             style={{ width: "20rem", height: "23rem" }}
           >
-            <img
+          <img
               src="/src/images/integrantes/santiago.jpg"
               alt="esteban"
-              className="fotoCircle mx-4 mt-4"
+              className="fotoCircle mt-4"
             />
-            <h3 className="text-center mt-4"><b>Santiago Castillo</b></h3>
+            <h3 className="text-center mt-4"><b>Santiago Aguirre</b></h3>
+          
           </div>
         </div>
 
@@ -84,9 +116,10 @@ function About() {
             <img
               src="/src/images/integrantes/alberto.png"
               alt="esteban"
-              className="fotoCircle mx-4 mt-4"
+              className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b> Alberto Aguirre</b></h3>
+            
           </div>
         </div>
       </div>
