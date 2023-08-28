76b2631fc30f6ab3a7ce76d5e7ae1b28289eb43e
DanielSpiner <danielspr.33@gmail.com>
2023-08-28 04:16:16 -0300

    Se agrega iconos, falta terminar el tamaño y acomodarlos.

--- VeterinariaHuellitas/src/pages/sobrenosotros/Sobrenosotros.jsx
+++ VeterinariaHuellitas/src/pages/sobrenosotros/Sobrenosotros.jsx
@@ -4,36 +4,17 @@ function About() {
   return (
     <>
       <div>
-      <h2 className="text-center my-4"><b><u>¿Quiénes somos?</u></b></h2>
+      <h1 className="text-center my-4"><b>¿Quiénes somos?</b></h1>
       </div>
 
-      {/* Contenedor 1 */}
+      
       <div className="container fach text-center p-5">
-        <p>
-          En Huellitas, comprendemos que tus mascotas son parte de tu familia.
-          Nuestro compromiso es proporcionarles el mejor cuidado veterinario en
-          un ambiente cálido y acogedor. Con años de experiencia en la atención
-          de animales de compañía, nuestro equipo de veterinarios altamente
-          capacitados se dedica a mantener a tus amigos peludos felices y
-          saludables en cada etapa de sus vidas.
-        </p>
-        <p>
-          Ofrecemos una amplia gama de servicios veterinarios diseñados para
-          satisfacer todas las necesidades de tus mascotas. Desde chequeos de
-          rutina, vacunación y desparasitación, hasta cirugías especializadas y
-          cuidados intensivos, estamos preparados para brindar la atención
-          médica que tus animales merecen. Ya sea que tengas un cachorro
-          juguetón, un gato curioso o una mascota mayor que necesita cuidados
-          especiales, estamos aquí para ayudar.
-        </p>
-        <p>
-          Nuestro equipo está formado por apasionados amantes de los animales
-          que se esfuerzan por establecer conexiones duraderas con nuestros
-          pacientes y sus dueños. Entendemos que cada mascota es única, y
-          trabajamos en colaboración contigo para desarrollar planes de atención
-          personalizados que se adapten a las necesidades individuales de tu
-          compañero peludo.
-        </p>
+     <p>
+     Somos un equipo de desarrolladores Full Stack apasionados por los animales y la tecnología.
+          Juntos, creamos 'Huellitas', una página de veterinaria que une nuestra experiencia en desarrollo con nuestro amor por los amigos peludos.
+          Con una interfaz amigable, permitimos a los dueños de mascotas acceder fácilmente a servicios veterinarios y programar citas.
+          Nuestra misión es mejorar la vida de las mascotas y sus dueños a través de la innovación tecnológica y el cuidado dedicado.
+     </p>
       </div>
 
       <div className="container my-5">
@@ -41,7 +22,7 @@ function About() {
       </div>
       {/* Fin de Contenedor 1 */}
 
-      <h2 className="text-center"><b><u>Nuestro equipo</u></b></h2>
+      <h1 className="text-center"><b>Nuestro equipo</b></h1>
 
       {/* Contenedor 2 */}
       <div className="container d-flex flex-wrap justify-content-center">
@@ -56,6 +37,12 @@ function About() {
               className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b>Pablo Decima</b></h3>
+            <div className="d-flex justify-content-evenly">
+            <i className="bi bi-linkedin"></i>
+            <i class="bi bi-github"></i>
+            
+            </div>
+            
         
           </div>
         </div>
@@ -71,6 +58,12 @@ function About() {
               className="fotoCircle  mt-4"
             />
             <h3 className="text-center mt-4"><b>Daniel Spiner</b></h3>
+
+            <div className="d-flex justify-content-evenly">
+            <i className="bi bi-linkedin"></i>
+            <i class="bi bi-github"></i>
+            
+            </div>
           
           </div>
         </div>
@@ -86,6 +79,12 @@ function About() {
               className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b>Esteban Pedraza</b></h3>
+
+            <div className="d-flex justify-content-evenly">
+            <i className="bi bi-linkedin"></i>
+            <i class="bi bi-github"></i>
+            
+            </div>
             
           </div>
         </div>
@@ -104,6 +103,12 @@ function About() {
               className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b>Santiago Aguirre</b></h3>
+
+            <div className="d-flex justify-content-evenly">
+            <i className="bi bi-linkedin"></i>
+            <i class="bi bi-github"></i>
+            
+            </div>
           
           </div>
         </div>
@@ -119,6 +124,12 @@ function About() {
               className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b> Alberto Aguirre</b></h3>
+
+            <div className="d-flex justify-content-evenly">
+            <i className="bi bi-linkedin"></i>
+            <i class="bi bi-github"></i>
+            
+            </div>
             
           </div>
         </div>
