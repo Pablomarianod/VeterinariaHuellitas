c10d7cfe7d3ed57de84d38dfaf612832accc6322
Pablo <pablomd_98@hotmail.com>
2023-08-29 19:21:37 -0300

    correciones en boton de redes y correcto linkeo hacia las mismas

--- VeterinariaHuellitas/src/pages/sobrenosotros/Sobrenosotros.jsx
+++ VeterinariaHuellitas/src/pages/sobrenosotros/Sobrenosotros.jsx
@@ -1,30 +1,39 @@
 import "./Sobrenosotros.css";
 
 function About() {
+
+  const handleLinkedInClick = (url) => {
+    window.open(url, '_blank');
+  };
+
+  const handleGitHubClick = (url) => {
+    window.open(url, '_blank');
+  };
+
   return (
     <>
       <div>
-      <h1 className="text-center my-4"><b>¿Quiénes somos?</b></h1>
+        <h1 className="text-center my-4"><b>¿Quiénes somos?</b></h1>
       </div>
 
-      
+
       <div className="container fach text-center p-5">
-     <p>
-     Somos un equipo de desarrolladores Full Stack apasionados por los animales y la tecnología.
+        <p>
+          Somos un equipo de desarrolladores Full Stack apasionados por los animales y la tecnología.
           Juntos, creamos 'Huellitas', una página de veterinaria que une nuestra experiencia en desarrollo con nuestro amor por los amigos peludos.
           Con una interfaz amigable, permitimos a los dueños de mascotas acceder fácilmente a servicios veterinarios y programar citas.
           Nuestra misión es mejorar la vida de las mascotas y sus dueños a través de la innovación tecnológica y el cuidado dedicado.
-     </p>
+        </p>
       </div>
 
       <div className="container my-5">
         <hr />
       </div>
-      {/* Fin de Contenedor 1 */}
 
-      <h1 className="text-center"><b>Nuestro equipo</b></h1>
 
-      {/* Contenedor 2 */}
+      <h1 className="text-center"><b>Nuestro equipo:</b></h1>
+
+
       <div className="container d-flex flex-wrap justify-content-center">
         <div className="d-flex mx-4 my-4">
           <div
@@ -33,17 +42,23 @@ function About() {
           >
             <img
               src="/src/images/integrantes/pablo.jpg"
-              alt="esteban"
+              alt="Pablo"
               className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b>Pablo Decima</b></h3>
             <div className="d-flex justify-content-evenly">
-            <i className="bi bi-linkedin mb-2"></i>
-            <i class="bi bi-github mb-2"></i>
-            
+            <i
+                className="bi bi-linkedin mb-2 botonIcono"
+                onClick={() => handleLinkedInClick("https://www.linkedin.com/in/pablo-mariano-d/")}
+              ></i>
+              <i
+                className="bi bi-github mb-2 botonIcono"
+                onClick={() => handleGitHubClick("https://github.com/Pablomarianod")}
+              ></i>
+
             </div>
-            
-        
+
+
           </div>
         </div>
 
@@ -54,17 +69,23 @@ function About() {
           >
             <img
               src="/src/images/integrantes/daniell.jpg"
-              alt="esteban"
+              alt="Daniel"
               className="fotoCircle  mt-4"
             />
             <h3 className="text-center mt-4"><b>Daniel Spiner</b></h3>
 
             <div className="d-flex justify-content-evenly">
-            <i className="bi bi-linkedin mb-2"></i>
-            <i class="bi bi-github mb-2"></i>
-            
+            <i
+                className="bi bi-linkedin mb-2 botonIcono"
+                onClick={() => handleLinkedInClick("https://www.linkedin.com/in/daniel-spiner-7b46a8271/")}
+              ></i>
+              <i
+                className="bi bi-github mb-2 botonIcono"
+                onClick={() => handleGitHubClick("https://github.com/danielspiner")}
+              ></i>
+
             </div>
-          
+
           </div>
         </div>
 
@@ -75,22 +96,28 @@ function About() {
           >
             <img
               src="/src/images/integrantes/Estebanp.jpg"
-              alt="esteban"
+              alt="Esteban"
               className="fotoCircle mt-4"
             />
-            
+
             <h3 className="text-center mt-4"><b>Esteban Pedraza</b></h3>
 
             <div className="d-flex justify-content-evenly colorIcon">
-            <i className="bi bi-linkedin mb-2"></i>
-            <i class="bi bi-github mb-2"></i>
-            
+            <i
+                className="bi bi-linkedin mb-2 botonIcono"
+                onClick={() => handleLinkedInClick("https://ar.linkedin.com/in/esteban-pedraza-82b938284")}
+              ></i>
+              <i
+                className="bi bi-github mb-2 botonIcono"
+                onClick={() => handleGitHubClick("https://github.com/esteban-82")}
+              ></i>
+
             </div>
-            
+
           </div>
         </div>
       </div>
-           
+
 
       <div className="container d-flex flex-wrap justify-content-center">
         <div className="d-flex mx-5 my-4">
@@ -98,19 +125,25 @@ function About() {
             className="card fach cambiocolor"
             style={{ width: "20rem", height: "23rem" }}
           >
-          <img
+            <img
               src="/src/images/integrantes/santiago.jpg"
-              alt="esteban"
+              alt="Santiago"
               className="fotoCircle mt-4"
             />
-            <h3 className="text-center mt-4"><b>Santiago Aguirre</b></h3>
+            <h3 className="text-center mt-4"><b>Santiago Castillo</b></h3>
 
             <div className="d-flex justify-content-evenly">
-            <i className="bi bi-linkedin mb-2"></i>
-            <i class="bi bi-github mb-2"></i>
-            
+            <i
+                className="bi bi-linkedin mb-2 botonIcono"
+                onClick={() => handleLinkedInClick("https://www.linkedin.com/")}
+              ></i>
+              <i
+                className="bi bi-github mb-2 botonIcono"
+                onClick={() => handleGitHubClick("https://github.com/SantiagoECastillo")}
+              ></i>
+
             </div>
-          
+
           </div>
         </div>
 
@@ -121,17 +154,23 @@ function About() {
           >
             <img
               src="/src/images/integrantes/alberto.png"
-              alt="esteban"
+              alt="Alberto"
               className="fotoCircle mt-4"
             />
             <h3 className="text-center mt-4"><b> Alberto Aguirre</b></h3>
 
             <div className="d-flex justify-content-evenly">
-            <i className="bi bi-linkedin mb-2"></i>
-            <i class="bi bi-github mb-2 "></i>
-            
+            <i
+                className="bi bi-linkedin mb-2 botonIcono"
+                onClick={() => handleLinkedInClick("https://www.linkedin.com/in/alberto-aguirre-691b5128a")}
+              ></i>
+              <i
+                className="bi bi-github mb-2 botonIcono"
+                onClick={() => handleGitHubClick("https://github.com/amalberto")}
+              ></i>
+
             </div>
-            
+
           </div>
         </div>
       </div>
